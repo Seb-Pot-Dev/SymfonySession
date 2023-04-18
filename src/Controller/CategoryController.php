39d1e804d57085c116211d465dbc31b7b5e5cf7d
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-04-18 10:09:14 +0200

    lister les catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+
+    //On appel le manager de doctrine 
+    public function index(ManagerRegistry $doctrine): Response
     {
+        //récupérer les Category de la base de donnees
+        $categories = $doctrine->getRepository(Category::class)->findAll();
+
+        //renvoie la vue et associe des données
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+            'categories' => $categories
         ]);
     }
+    
 }
